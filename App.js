1db2174be11d1e3ade3baeea105ae02f74f2856b
parindyawijeseskara <parindyawijesekara@gmail.com>
2022-10-04 12:54:53 +0530

    Updated Content

--- App.js
+++ App.js
@@ -1,7 +1,4 @@
-import { View, Text, Button} from 'react-native'
 import React from 'react'
-import Home from './screens/Home';
-import OnboardingScreen from './screens/OnboardingScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Welcome from './screens/Welcome';
@@ -15,8 +12,6 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      {/* <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
-      <Stack.Screen name="Home" component={Home} /> */}
       <Stack.Screen name="Welcome" component={Welcome} options={{headerShown:false}}/>
       <Stack.Screen name="SignIn" component={SignIn} options={{headerShown:false}}/>
       <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false}}/>
